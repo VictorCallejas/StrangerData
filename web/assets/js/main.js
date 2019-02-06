511ddcaf42f6666c7cb3b2bc50145e90904f992b
VictorCallejas <victorcf999@gmail.com>
2019-02-07 00:20:19 +0100

    Testing button contact us

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,8 +1,6 @@
-$(document).ready(function(){
-	document.getElementsById('btn-contact').click(function(){
-	document.getElementsByClassName('modal1').style.display = "flex";
-	document.getElementsById('email_div').fadeIn(100)})
-	document.getElementsByClassName('cerrar_contact').click(function(){
-	document.getElementsByClassName('modal1').style.display = "none";
-	document.getElementsById('email_div').fadeOut(300)})
-});
+document.getElementsById('btn-contact').click(function(){
+document.getElementsByClassName('modal1').style.display = "flex";
+document.getElementsById('email_div').fadeIn(100)})
+document.getElementsByClassName('cerrar_contact').click(function(){
+document.getElementsByClassName('modal1').style.display = "none";
+document.getElementsById('email_div').fadeOut(300)})
