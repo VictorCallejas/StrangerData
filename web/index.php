67e1e6561fbd39f3e991afa4356f3be0d95abd9f
Victor Callejas Fuentes <victorcf999@gmail.com>
2019-02-06 12:42:41 +0100

    Muchas cosas

--- web/index.php
+++ web/index.php
@@ -3,12 +3,58 @@
 
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Prueba</title>
+
+        <title>Stranger Data</title>
+
+        <!-- FAVICON -->
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/...">
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/...">
     </head>
 
     <body>
-        <h1>Prueba 1</h1>
+    <header>
+	   	<a href="index.php" id="home"><img src="images/home.png" alt="Inicio"></a>
+		<nav>
+			<a href="#" class="btn-nav">Aplicacion</a>
+			<a href="...php" class="btn-nav">Proyecto</a>
+			<a href="...php" class="btn-nav">Equipo</a>
+		</nav>
+	</header>
+	<!-- PROGRESS BAR -->
+	<progress value="0" id="progressBar">
+  		<div class="progress-container">
+   		   <span class="progress-bar"></span>
+ 	    </div>
+    </progress>
+    
+    <div class="aplication-main">
+		<div class="mapa">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3127.1803697518367!2d-0.505647974955759!3d38.391078400989464!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x914b2101c952d709!2sPELUQUERIA+Rosa+Fuentes!5e0!3m2!1sen!2ses!4v1501801979653" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <div class="filtros">
+				<div>
+                    Filtros
+                </div>
+        </div>
+    </div>
+    
+    <!-- FOOTER -->
+    <footer>
+        <img src="assets/img/logo-cajamarhack.png" alt="Cajamar UniversityHack 2019">
+        <img src="assets/img/logo-datalab.png" alt="Cajamar Datalab">
+        <div>
+            <p>Your best path</p>
+            <a href="mail.php" class="btn-nav">Contact us</a>
+        </div>
+        <img src="assets/img/logo_etsinf.png" alt="Escuela Superior de ingenieria informatica">
+        <img src="assets/img/logo_upv.png" alt="Universidad Politecnica de alencia">
+    </footer>
+    <!-- SCRIPTS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/aplication.css">
+	<script type="text/javascript" src="js/main.js"></script>
     </body>
 <?php echo "hola mundo";?>
 </html>
