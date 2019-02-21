7988d2cf4fe0ce258877162b5be07471554edf52
Joaquin Montesinos <jkm280294@gmail.com>
2019-02-21 11:05:11 +0100

    last version

--- web/dashboard/respuesta.php
+++ web/dashboard/respuesta.php
@@ -5,11 +5,25 @@ $destination_latitud = $_GET["destination_latitud"];
 $destination_longitud = $_GET["destination_longitud"];
 $origin = $origin_latitud.",".$origin_longitud;
 $destination = $destination_latitud.",".$destination_longitud;
-
-
+ 
+$andando = $_GET["andando"];
+$coche = $_GET["coche"];
+$transporte_publico = $_GET["transporte_publico"];
+$bici = $_GET["bici"];
+if ($andando=="si"){
+	$mode = "walking";
+}else if ($coche=="si"){
+	$mode = "driving";
+}else if ($transporte_publico=="si"){
+	$mode = "driving";
+} else if ($bici=="si"){
+	$mode = "bicycling";
+}else{
+	$mode = "walking";
+}
 /*$origin = "39.4688751,-3.3976791";
 $destination = "39.4688751,-3.4976792"; */
-$mode="walking"; //"available_travel_modes" : [ "DRIVING", "BICYCLING", "WALKING" ],
+//$mode="walking"; //"available_travel_modes" : [ "DRIVING", "BICYCLING", "WALKING" ],
 $request="https://maps.googleapis.com/maps/api/directions/json?origin=";
 $request.=$origin;
 $request.="&destination=";
@@ -17,21 +31,16 @@ $request.=$destination;
 $request.="&mode=";
 $request.=$mode;
 $request.="&mode=walking&key=AIzaSyA_i6oWqxHiZThh7uzo9UZxg3gShyhqGvU";
-
-
  
 $jsondata = file_get_contents($request);
 $response_a = json_decode($jsondata);
 $datanumber =  sizeof($response_a->routes[0]->legs[0]->steps);
-
-
 for ($i = 0; $i < $datanumber; $i++) {
   $steps[$i] = (string)$response_a->routes[0]->legs[0]->steps[$i]->start_location->lat;
   $steps[$i] .= ",";
   $steps[$i] .= (string)$response_a->routes[0]->legs[0]->steps[$i]->start_location->lng;
 }
 $steps[$datanumber] = $destination;
-
 echo json_encode($steps);
  
 ?>
