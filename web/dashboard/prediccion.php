0b360dc9aee086ebbf214899d05fca43fc34e701
Joaquin Montesinos <jkm280294@gmail.com>
2019-02-21 11:04:22 +0100

    last version

--- web/dashboard/prediccion.php
+++ web/dashboard/prediccion.php
@@ -7,10 +7,13 @@
 <div id="filtros">
 	<div id="filtros-box">
 	
+
+
+
 	<h2> Ubicación: </h2>
 	<div class="grupo">
 	<div class="pretty p-switch p-fill">
-        <input type="radio" name="switch11" id="Ubicacion"/>
+        <input type="radio" name="switch11" id="Ubicacion" />
         <div class="state p-success">
             <label>Usar ubicacion actual</label>
         </div>
@@ -123,7 +126,6 @@
 		
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_i6oWqxHiZThh7uzo9UZxg3gShyhqGvU"></script>
 
-
 <script>   
 var map;
 var layer;
@@ -147,15 +149,15 @@ var request_URL_nearest_length;
 var account_time=[];
 //////////// parametros back
 var ubicado="no"; //si es si, significa que coge la latitud y longitud, si es no, solo muestra cajeros de españa dependiendo de los parametros que elija, si/no
-var libreta_url="no"; //cajeros que permiten libreta, si/no
-var ingreso_url="no"; //cajeros que permiten ingreso, si/no
-var recibos_url="no"; //cajeros que permiten recibos, si/no
+var libreta_url="si"; //cajeros que permiten libreta, si/no
+var ingreso_url="si"; //cajeros que permiten ingreso, si/no
+var recibos_url="si"; //cajeros que permiten recibos, si/no
 var contactless_url="no"; //cajeros que permiten contactless, si/no
-var transferencia_url="no"; //cajeros que permiten transferencias, si/no
-var extraer_url="no"; //cajeros que permiten extraer, si/no
-var con_saldo_url="no"; //cajeros que permiten contactless, si/no
-var busca_oficina_url="no"; //el usuario está buscanto oficinas, si/no
-var busca_cajero_url="si"; //el usuario está buscanto cajeros, si/no
+var transferencia_url="si"; //cajeros que permiten transferencias, si/no
+var extraer_url="si"; //cajeros que permiten extraer, si/no
+var con_saldo_url="si"; //cajeros que permiten contactless, si/no
+var busca_oficina_url="si"; //el usuario está buscanto oficinas, si/no
+var busca_cajero_url="no"; //el usuario está buscanto cajeros, si/no
 var pertenece_banco_url="cajamar"; //el usuario pertenece al banco ... cajamar,santander, ...
 var busca_bancos_url="cajamar"; //el usuario solo busca bancos de la entidad... ???? este no se si ponerlo
 var dispuesto_url=0.65; //dispuesto a pagar de comision, 0, 0.65, 2 
@@ -169,10 +171,11 @@ var destination_latitud_val=[];
 var destination_longitud_val=[];
 var destination_latitud=0;
 var destination_longitud=0;
-var coche_url="no";
-var andando_url="si";
+var coche_url="si";
+var andando_url="no";
 var transporte_publico_url="no";
 var bici_url="no"; 
+var marker_value=0;
 var bound_activo=0;
 var URL; 
 var bounds;
@@ -188,7 +191,7 @@ var URL_no="https://qmiets.tk:5000/nearest?ubicado=no&latitud="+origin_latitud+"
 var URL_si="https://qmiets.tk:5000/nearest?ubicado=si&latitud="+origin_latitud+"&longitud="+origin_longitud+"&libreta="+libreta_url+"&ingreso="+ingreso_url+"&recibos="+recibos_url+"&contactless="+contactless_url+"&transferencia="+transferencia_url+"&extraer="+extraer_url+"&con_saldo="+con_saldo_url+"&busca_oficina_url="+busca_oficina_url+"&busca_cajero_url="+busca_cajero_url+"&pertenece_banco_url="+pertenece_banco_url+"&busca_bancos_url="+busca_bancos_url+"&dispuesto_url="+dispuesto_url+"&hora_url="+hora_url+"&dia_url="+dia_url+"&numero_recomendaciones="+numero_recomendaciones;
 
 /////// api front
-var URL_nearest="https://strangerdata.club/dashboard/respuesta.php?origin_latitud="+origin_latitud+"&origin_longitud="+origin_longitud+"&destination_latitud="+destination_latitud+"&destination_longitud="+destination_longitud+"&andando="+andando_url+"coche="+coche_url+"transporte_publico="+transporte_publico_url+"bici="+bici_url+"&hora_url="+hora_url+"&dia_url="+dia_url;
+var URL_nearest="http://localhost:8888/StrangerData/web/dashboard/respuesta.php?origin_latitud="+origin_latitud+"&origin_longitud="+origin_longitud+"&destination_latitud="+destination_latitud+"&destination_longitud="+destination_longitud+"&andando="+andando_url+"coche="+coche_url+"transporte_publico="+transporte_publico_url+"bici="+bici_url+"&hora_url="+hora_url+"&dia_url="+dia_url;
  var URL_nearest_time="https://strangerdata.club/dashboard/respuesta.php?origin_latitud="+origin_latitud+"&origin_longitud="+origin_longitud+"&destination_latitud="+destination_latitud+"&destination_longitud="+destination_longitud+"&andando="+andando_url+"coche="+coche_url+"transporte_publico="+transporte_publico_url+"bici="+bici_url+"&hora_url="+hora_url+"&dia_url="+dia_url;
 
  
@@ -204,10 +207,10 @@ function actualizarURL_no(){
 }
 
 function actualizar_nearest(){
-	URL_nearest="https://strangerdata.club/dashboard/respuesta_tiempo.php?origin_latitud="+origin_latitud+"&origin_longitud="+origin_longitud+"&destination_latitud="+destination_latitud+"&destination_longitud="+destination_longitud+"&andando="+andando_url+"coche="+coche_url+"transporte_publico="+transporte_publico_url+"bici="+bici_url+"&hora_url="+hora_url+"&dia_url="+dia_url;
+	URL_nearest="http://localhost:8888/StrangerData/web/dashboard/respuesta_tiempo.php?origin_latitud="+origin_latitud+"&origin_longitud="+origin_longitud+"&destination_latitud="+destination_latitud+"&destination_longitud="+destination_longitud+"&andando="+andando_url+"coche="+coche_url+"transporte_publico="+transporte_publico_url+"bici="+bici_url+"&hora_url="+hora_url+"&dia_url="+dia_url;
 }
 function actualizar_nearest_tiempo(){
-	 URL_nearest_time="https://strangerdata.club/dashboard/respuesta_tiempo.php?origin_latitud="+origin_latitud+"&origin_longitud="+origin_longitud+"&destination_latitud="+destination_latitud+"&destination_longitud="+destination_longitud+"&andando="+andando_url+"coche="+coche_url+"transporte_publico="+transporte_publico_url+"bici="+bici_url+"&hora_url="+hora_url+"&dia_url="+dia_url;
+	 URL_nearest_time="http://localhost:8888/StrangerData/web/dashboard/respuesta_tiempo.php?origin_latitud="+origin_latitud+"&origin_longitud="+origin_longitud+"&destination_latitud="+destination_latitud+"&destination_longitud="+destination_longitud+"&andando="+andando_url+"coche="+coche_url+"transporte_publico="+transporte_publico_url+"bici="+bici_url+"&hora_url="+hora_url+"&dia_url="+dia_url;
 }
 //// funcion text to min
 function to_min(s){
@@ -234,18 +237,17 @@ function to_min(s){
 
 function maestra(URL){
       //actualiza parametros de la url
-      actualizarURL_si();
-      //actualizarURL_no();
-
+       actualizarURL_si();
+      actualizarURL_no();
       //llama a al api con los parametros de origen
       request_URL_nearest_length = $.get(URL, function(ss){
-		console.log(ss);
+        console.log(ss);
       });
       //cuando se tenga respuest, entoces selecciona lat y long que seran de destino
       $.when(request_URL_nearest_length).done(function(msg) {
           for(var i=0; i<request_URL_nearest_length.responseJSON.cajeros.length; i++){
-             destination_latitud_val[i] = request_URL_nearest_length.responseJSON.cajeros[i].latitud; //valeria
-           destination_longitud_val[i] = request_URL_nearest_length.responseJSON.cajeros[i].longitud; //valeria
+             destination_latitud_val[i] = request_URL_nearest_length.responseJSON.cajeros[i].latitud;  
+           destination_longitud_val[i] = request_URL_nearest_length.responseJSON.cajeros[i].longitud;  
           }
           //llamo x n veces al api de google para obtener tiempo y camino
           for(var i=0; i<request_URL_nearest_length.responseJSON.cajeros.length; i++){       
@@ -271,12 +273,14 @@ function maestra(URL){
                    }
   
           //selecciono el cajero mas cercano, sabiendo la posicion del array del mejor ponderado
- 
+                   
                   for(var i=0; i<request_duration_URL_nearest.length; i++){
                     if(i==account_time.indexOf(Math.max.apply(Math,account_time))){
                        jArray = JSON.parse(request_URL_nearest[i].responseText);
                     }                       
                   }
+                  markers=[];
+                  bounds = new google.maps.LatLngBounds();
           /// todos los puntos del camino.
                   for(var i=0; i<jArray.length; i++){
                       var aux = jArray[i];
@@ -287,20 +291,41 @@ function maestra(URL){
                       longitud_makers[i] = long;
                       markers[i] = new google.maps.LatLng(latitud_makers[i], longitud_makers[i]); 
                       bounds.extend(markers[i]);
-
                    }
           ///punto unicial y final
                  punto_final_no_tocar = {lat: latitud_makers[jArray.length-1], lng: longitud_makers[jArray.length-1]};
                  punto_inicial_no_tocar = {lat: latitud_makers[0], lng: longitud_makers[0]};
           ////inicializo el mapa
+          ubicado="si";
           bound_activo=1;
-                 initMap();
-
+          marker_value=1;
+                 initMap();       
           });
         });
 }
-
-function maestra_no_ubicado(URL){}
+function maestra_no_ubicado(URL){
+//actualiza parametros de la url
+       actualizarURL_si();
+      actualizarURL_no();
+      //llama a al api con los parametros de origen
+      request_URL_nearest_length = $.get(URL, function(ss){
+        console.log(ss);
+      });
+      //cuando se tenga respuest, entoces selecciona lat y long que seran de destino
+      $.when(request_URL_nearest_length).done(function(msg) {
+          
+          for(var i=0; i<request_URL_nearest_length.responseJSON.cajeros.length; i++){
+             destination_latitud_val[i] = request_URL_nearest_length.responseJSON.cajeros[i].latitud;  
+           destination_longitud_val[i] = request_URL_nearest_length.responseJSON.cajeros[i].longitud; 
+           }
+           if(ubicado=="no"){
+            polyline.setMap(null);
+           }
+           ubicado="no";
+           marker_value=1;
+           initMap(); 
+          });
+}
  
 //guardo setup de las llamadas ajax, para que no me almacene en cache.
 $.ajaxSetup({cache: false});
@@ -312,14 +337,27 @@ function showPosition(position) {
    
 }
 
+
+
+$(document).ready(function(){
+    $(".check-male").click(function(){
+        $("#male").prop("checked", true);
+    });
+     
+});
+
+
 /*******Ubicacion********/
 //Ubicacion  
 $(document).ready(function() {
     //set initial state.
     $('#Ubicacion').change(function() {
+      console.log("entra");
         if($(this).is(":checked")) {
+
             ubicado="si"; 
             if (navigator.geolocation) {
+              console.log(origin_latitud);
                navigator.geolocation.getCurrentPosition(showPosition);
 
 
@@ -483,9 +521,7 @@ $(document).ready(function() {
 
 
 
-
 <script> 
-
 ///mapa
 function initMap() {
     var customMapTypeId = "night_mode_style"
@@ -508,13 +544,9 @@ function initMap() {
     }else{
       map.fitBounds(bounds);
     }
-
-
-
    var iconsetngs = {
         path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
      };
-
     var polylineoptns = {
         strokeOpacity: 0.8,
         strokeWeight: 3,
@@ -525,50 +557,62 @@ function initMap() {
             icon: iconsetngs,
             offset: '100%'}]
     };
- 
-
+   
     polyline = new google.maps.Polyline(polylineoptns);
     
-
     var z = 0;
     
     var path = [];
         path[z] = polyline.getPath();
-
      
-     var markador = new google.maps.Marker({
+   var markador2 = new google.maps.Marker({
     position: punto_final_no_tocar,
-    icon: { url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
-    map: map
+    icon: { url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
+    map: map,
+    title: 'Punto recomendado'
    });
    var markador = new google.maps.Marker({
     position: punto_inicial_no_tocar,
-    icon: { url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
-    map: map
+    icon: { url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"},
+    map: map,
+    title: 'Punto de partida'
    });
+    var contentString ='<p>Punto de partida</p>';  ////sale en blanco. Como poner en negro!!
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+     });
+    markador.addListener('click', function() {
+      infowindow.open(map, markador);
+    });
  
-
-    for ( i = 0 ; i < markers.length; i++){ //LOOP TO DISPLAY THE MARKERS
+    for (var i =  0 ; i < markers.length; i++){ //LOOP TO DISPLAY THE MARKERS
         var pos = markers[i];
           var marker = new google.maps.Marker({
             position: pos,             
-            icon: { url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
+            icon: { url: "http://maps.google.com/mapfiles/ms/icons/none.png"},
             map: map
         });
        path[z].push(marker.getPosition()); //PUSH THE NEWLY CREATED MARKER'S POSITION TO THE PATH ARRAY
     }
  
-  
+  if(marker_value!=0){
 
-
+      for(var i=0; i<request_URL_nearest_length.responseJSON.cajeros.length; i++){
+            if(request_URL_nearest_length.responseJSON.cajeros[i].tipo=="oficina"){
+               showMarkerByLatLng(new google.maps.LatLng(destination_latitud_val[i],destination_longitud_val[i]), false); 
+              }else{
+               showMarkerByLatLng2(new google.maps.LatLng(destination_latitud_val[i],destination_longitud_val[i]), false); 
+              } 
+          }
+  }else{
+  }
+    
   function showMarkerByLatLng(pos, infoWindowTitle,infoWindowText,imageMarker){
-
       var myMarker1 = new google.maps.Marker();
       
       myMarker1.setMap(map);
       
       myMarker1.setPosition(pos);
-
       if(imageMarker!=false){
       myMarker1.setIcon(imageMarker);
       }
@@ -581,29 +625,23 @@ function initMap() {
               infoWindowText+
               '</div>' +
               '</div>';
-
       var myMarker1InfoWindow = new google.maps.InfoWindow({
           content: myMarker1ContentString
       });
-
       google.maps.event.addListener(myMarker1, 'click', function() {
           myMarker1InfoWindow.open(map, myMarker1);
       });
+
   }
-
-
-
   function showMarkerByLatLng2(pos, infoWindowTitle,infoWindowText,imageMarker2){
-
      
       var myMarker1 = new google.maps.Marker();
       
       myMarker1.setMap(map);
       
       myMarker1.setPosition(pos);
-
       if(imageMarker2!=false){
-      myMarker1.setIcon("https://maps.google.com/mapfiles/ms/icons/blue-dot.png");
+      myMarker1.setIcon("http://maps.google.com/mapfiles/ms/icons/blue-dot.png");
       }
       
    
@@ -611,10 +649,9 @@ function initMap() {
           myMarker1InfoWindow.open(map, myMarker1);
       });
   }
+        console.log("entra");
+
 }
  
  window.onload = initMap;
-
 </script>
-
- 
